caada9176188d714e6116214c3721dbd6f93f647
Ayush Mehra <ayushme53@gmail.com>
2020-05-14 13:04:28 +0530

    cf

---
+++ 1003A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define faster ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define mp make_pair
+#define mod 1000000007
+#define qmod 998244353
+#define endl "\n"
+#define pb push_back
+#define ff first
+#define ss second
+ 
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+ 
+const int MOD = 1e9 + 7;
+const int INF = 1e9 + 5;
+const ll LINF = LLONG_MAX;
+
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    map<int,int>mp;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+        mp[a[i]]++;        
+    }
+    int mx=0;
+    for(auto x: mp){
+        mx = max(mx,x.second);
+    }
+    cout<<mx<<endl;
+}
