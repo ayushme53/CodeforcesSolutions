4f80670a51082234b0aa4eb44cd851d418abd7e7
Ayush Mehra <ayushme53@gmail.com>
2020-05-17 16:34:34 +0530

    cf

---
+++ 545D.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define faster ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define mp make_pair
+#define mod 1000000007
+#define qmod 998244353
+#define endl "\n"
+#define pb push_back
+#define ff first
+#define ss second
+ 
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+ 
+const int MOD = 1e9 + 7;
+const int INF = 1e9 + 5;
+const ll LINF = LLONG_MAX;
+
+int main(){
+    ll n;
+    cin>>n;
+    ll a[n];
+    for(ll i=0;i<n;i++){
+        cin>>a[i];
+    }
+    sort(a,a+n);
+    ll sum=0,c=0;
+    for(ll i=0;i<n;i++){
+        if(a[i]>=sum){
+            c++;
+            sum +=a[i];
+        }
+    }
+    cout<<c<<endl;
+    
+}
