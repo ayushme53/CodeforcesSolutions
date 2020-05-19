c918d5745c1e2923156a8c090573989db05e6a37
Ayush Mehra <ayushme53@gmail.com>
2020-05-19 15:37:02 +0530

    cf

---
+++ 1352A.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define faster ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define mp make_pair
+#define mod 1000000007
+#define qmod 998244353
+#define endl "\n"
+#define pb push_back
+#define ff first
+#define ss second
+ 
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+ 
+const int MOD = 1e9 + 7;
+const int INF = 1e9 + 5;
+const ll LINF = LLONG_MAX;
+
+int main(){
+/*	#ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin); 
+    freopen("output.txt","w",stdout);   
+    #endif */
+    
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		int x=1,y;
+		vi a;
+		while(x<=n){
+			y = (n/x)%10;
+			if(y)
+				a.pb(y*x);
+			x*=10;
+		}
+		cout<<a.size()<<endl;
+		for(auto x:a)
+			cout<<x<<" ";
+		cout<<endl;
+	}	
+
+}
