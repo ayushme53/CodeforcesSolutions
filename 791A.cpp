8982b6a1d889b6364d548c8b1663dedc451a19c3
Ayush Mehra <ayushme53@gmail.com>
2020-05-28 11:22:29 +0530

    cf

---
+++ 791A.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define faster ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define mp make_pair
+#define mod 1000000007
+#define qmod 998244353
+#define endl "\n"
+#define pb push_back
+#define ff first
+#define ss second
+#define mk make_pair 
+ 
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<string> vs;
+ 
+const int MOD = 1e9 + 7;
+const int INF = 1e9 + 5;
+const ll LINF = LLONG_MAX;
+const int MAX_V = 201;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	int answer = 0;
+	while(a <= b) {
+		a *= 3;
+		b *= 2;
+		++answer;
+	}
+	printf("%d\n", answer);
+}
