cde684f0bc4b901281ac42fdde180f039b243b94
Ayush Mehra <ayushme53@gmail.com>
2020-09-06 10:45:51 +0530

    cf

---
+++ 1237A.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define faster ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define mp make_pair
+#define mod 1000000007
+#define qmod 998244353
+#define endl "\n"
+#define pb push_back
+#define ff first
+#define ss second
+ 
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+ 
+const int MOD = 1e9 + 7;
+const int INF = 1e9 + 5;
+const ll LINF = LLONG_MAX;
+
+int main(){
+    faster;
+    int n;
+    cin >> n;
+    int flag = 1;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        if (x % 2 == 0) {
+            cout << x / 2 << '\n';
+        }
+       else {
+        cout << (x + flag) / 2 << '\n';
+        flag *= -1;
+        }
+    }
+}
