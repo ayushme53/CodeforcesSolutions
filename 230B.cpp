bf71693587fddf601844cf2607810421da0051b8
Ayush Mehra <ayushme53@gmail.com>
2020-09-07 22:36:46 +0530

    cf

---
+++ 230B.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define faster ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define mp make_pair
+#define mod 1000000007
+#define qmod 998244353
+#define endl "\n"
+#define pb push_back
+#define ff first
+#define ss second
+ 
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+ 
+const int MOD = 1e9 + 7;
+const int INF = 1e9 + 5;
+const ll LINF = LLONG_MAX;
+
+bool isPrime(int n) 
+{ 
+    // Corner cases 
+    if (n <= 1) 
+        return false; 
+    if (n <= 3) 
+        return true; 
+  
+    // This is checked so that we can skip 
+    // middle five numbers in below loop 
+    if (n % 2 == 0 || n % 3 == 0) 
+        return false; 
+  
+    for (int i = 5; i * i <= n; i = i + 6) 
+        if (n % i == 0 || n % (i + 2) == 0) 
+            return false; 
+  
+    return true; 
+} 
+
+int main(){
+	faster;
+	int n;
+	cin>>n;
+	
+	for(int i=0;i<n;i++){
+		 ll x;
+         cin>>x;
+         ll y=sqrt(x);
+         if (isPrime(y) && y*y==x) cout<<"YES\n";
+         else cout<<"NO\n";
+
+	}
+}
